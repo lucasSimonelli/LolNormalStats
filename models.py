4b417b733431b0f50ad81ae7e8ed3413e165f924
lucasSimonelli <lucasp.simonelli@gmail.com>
2013-11-11 14:23:54 -0300

    Added database through elixir

---
+++ models.py
@@ -0,0 +1,16 @@
+from elixir import *
+
+metadata.bind = "sqlite:///database.sqlite"
+metadata.bind.echo = True
+
+
+class Match(Entity):
+    champion = Field(Unicode(20))
+    gameType = Field(Unicode(30))
+    winLoss = Field(Unicode(30))
+    kills = Field(Integer, default=0)
+    deaths = Field(Integer, default=0)
+    assists = Field(Integer, default=0)
+    gold = Field(Float, default=0.0)
+    minions = Field(Integer, default=0)
+
