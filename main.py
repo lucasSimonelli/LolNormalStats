f5b6c6a45b5797101d1cf4f6e2034b1a0bd54af3
lucasSimonelli <lucasp.simonelli@gmail.com>
2013-11-12 12:10:07 -0300

    Main view behaviour moved from main -> view.view

--- main.py
+++ main.py
@@ -1,70 +1,14 @@
 #Libs
-import requests, tkMessageBox, json
-from lxml import html
+import tkMessageBox, json
 from Tkinter import *
 import ttk
 #My packages
 from database.queries import getChampionStats
-from database.updates import updateGameTypeSpecificStats
+from database.updates import loadNewMatches
 from database.models import *
 from utilities.constants import constants, champions
 from utilities.inout import printHtmlTable
-from utilities.misc import findBetween
-
-
-
-#Harcoded to lolking html structure
-def getChampionName(detail):
-	return detail[0][0][0][0].get('href')[11:]
-
-def matchWasWon(detail):
-	if detail[1][0][1].text==constants['winText'] :
-		return True
-	return False
-
-def loadNewMatches(js):
-	r = requests.get(js['lolkingUrl'])
-
-	historyHTML = findBetween(r.text,constants['startTrim'],constants['endTrim'])
-	historyHTML = historyHTML.rstrip('\n')[3:-2] #Removing \n hardcodily.
-	parsed = html.fromstring(historyHTML)
-
-	for match in parsed:
-		gameID = int(match.get(constants['dataGameIdClass']))
-		#Check if match is already in db
-		if not Match.query.filter_by(gameID=gameID).count():
-			for detail in match:
-				#ignoring extended details for now
-				if detail.get('class')==constants['detailsExtended']:
-					continue
-
-				#1st cell: champion
-				champion = getChampionName(detail)
-
-				#2nd cell: game type & w/l
-				gameType = detail[1][0][0].text
-
-				#2nd cell: game type
-				won = matchWasWon(detail)
-
-				#3rd cell: duration <ignored>
-
-				#4th cell: kda
-				kills = int(detail[3][0][0].text)
-				deaths = int(detail[3][0][3].text)
-				assists = int(detail[3][0][6].text)
-				
-				#4th cell: gold
-				gold = int(float(detail[4][0][0].text[0:-1])*1000)
-
-				#5th cell: minions
-				minions = int(detail[5][0][0].text)
-
-				dbObject = Match(champion=champion, gameID=gameID, gameType=gameType,won=won,kills=kills,deaths=deaths,assists=assists,gold=gold,minions=minions)
-				updateGameTypeSpecificStats(dbObject,gameType)
-				session.add(dbObject)
-
-	session.commit()
+from view.view import MainWindow
 
 js={}
 try:
@@ -76,63 +20,10 @@ except IOError:
 	
 
 
-
-master = Tk('LolNormalStats')
-master.title('LolNormalStats')
-w=master.winfo_screenwidth()
-h=master.winfo_screenheight()
-master.geometry("300x100+%d+%d" % ( (w-300)/2, (h-100)/2 ) )
-w = Label(master, text="Your lolking url:")
-w.grid(columnspan=2)
-
-e = Entry(master, width=50)
-e.insert(0, js['lolkingUrl'])
-e.grid(columnspan=2)
-
-
-
-e.focus_set()
-
 setup_all()
 create_all()
-def callback():
-	try:
-		js['lolkingUrl']=e.get()
-		tkMessageBox.showinfo(
-			"Ok Url",
-			"Url loaded successfully"
-		)
-	except requests.exceptions.MissingSchema:
-		tkMessageBox.showerror(
-			"Wrong Url",
-			"Cannot open provided url"
-		)
-
-def getData():
-	try:
-		pb1 = ttk.Progressbar(master, mode='determinate', name='pb1')
-		loadNewMatches(js)
-		tkMessageBox.showinfo(
-			"Ok Url",
-			"data downloaded successfully"
-		)
-	except requests.exceptions.MissingSchema:
-		tkMessageBox.showerror(
-			"Wrong Url",
-			"Cannot open provided url"
-		)
-
-b = Button(master, text="Load lolking url", command=callback)
-b.grid(row=2, column=0)
-
-
-b2 = Button(master, text="Get data", command=getData)
-b2.grid(row=2, column=1)
-
-
-mainloop()
-
-
+window=MainWindow(js)
+window.mainloop()
 
 l=[]
 firstTime=True
@@ -151,6 +42,7 @@ for champion in champions:
 	l.append(dict.values())
 printHtmlTable(l)
 
+js['lolkingUrl']=window.getUpdatedLolkingUrl()
 config = open('config.json','w')
 json.dump(js, config, sort_keys=True, indent=4)
 config.close()
--- view/view.py
+++ view/view.py
@@ -1,56 +1,64 @@
 #Libs
 import tkMessageBox
 from Tkinter import *
-import ttk
+import ttk, requests #Todo: redefine exception in inout and import that
 
-def callback():
-	try:
-		js['lolkingUrl']=e.get()
-		tkMessageBox.showinfo(
-			"Ok Url",
-			"Url loaded successfully"
-		)
-	except requests.exceptions.MissingSchema:
-		tkMessageBox.showerror(
-			"Wrong Url",
-			"Cannot open provided url"
-		)
+#Packages
+from database.updates import loadNewMatches
 
-def getData():
-	try:
-		loadNewMatches(js)
-		tkMessageBox.showinfo(
-			"Ok Url",
-			"data downloaded successfully"
-		)
-	except requests.exceptions.MissingSchema:
-		tkMessageBox.showerror(
-			"Wrong Url",
-			"Cannot open provided url"
-		)
 
 class MainWindow:
 	def __init__(self,js):
-		master = Tk('LolNormalStats')
-		master.title('LolNormalStats')
-		w=master.winfo_screenwidth()
-		h=master.winfo_screenheight()
-		master.geometry("300x100+%d+%d" % ( (w-300)/2, (h-100)/2 ) )
-		w = Label(master, text="Your lolking url:")
+		self.js = js
+		self.master = Tk('LolNormalStats')
+		self.master.title('LolNormalStats')
+		w=self.master.winfo_screenwidth()
+		h=self.master.winfo_screenheight()
+		self.master.geometry("300x100+%d+%d" % ( (w-300)/2, (h-100)/2 ) )
+		w = Label(self.master, text="Your lolking url:")
 		w.grid(columnspan=2)
 
-		e = Entry(master, width=50)
-		e.insert(0, js['lolkingUrl'])
-		e.grid(columnspan=2)
+		self.e = Entry(self.master, width=50)
+		self.e.insert(0, js['lolkingUrl'])
+		self.e.grid(columnspan=2)
 
-		e.focus_set()
+		self.e.focus_set()
 
-		b = Button(master, text="Load lolking url", command=callback)
+		b = Button(self.master, text="Load lolking url", command=self.callback)
 		b.grid(row=2, column=0)
 
 
-		b2 = Button(master, text="Get data", command=getData)
+		b2 = Button(self.master, text="Get data", command=self.getData)
 		b2.grid(row=2, column=1)
 
 	def mainloop(self):
 		mainloop()
+
+	def getData(self):
+		try:
+			loadNewMatches(self.js)
+			tkMessageBox.showinfo(
+				"Ok Url",
+				"data downloaded successfully"
+			)
+		except requests.exceptions.MissingSchema:
+			tkMessageBox.showerror(
+				"Wrong Url",
+				"Cannot open provided url"
+			)
+
+	def callback(self):
+		try:
+			self.js['lolkingUrl']=self.e.get()
+			tkMessageBox.showinfo(
+				"Ok Url",
+				"Url loaded successfully"
+			)
+		except requests.exceptions.MissingSchema:
+			tkMessageBox.showerror(
+				"Wrong Url",
+				"Cannot open provided url"
+			)
+
+	def getUpdatedLolkingUrl(self):
+		return self.js['lolkingUrl']
