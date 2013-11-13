c170f50b4194a690fc9faa764ff51452ad1b48ff
lucasSimonelli <lucasp.simonelli@gmail.com>
2013-11-13 14:06:09 -0300

    Added resources to cx-freeze build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os","lxml","sqlalchemy","requests","Tkinter","tkMessageBox","json"]}
+build_exe_options = {"packages": ["os","lxml","sqlalchemy","requests","Tkinter","tkMessageBox","json"], "include_files":['img/','templates/'],"silent":1}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
